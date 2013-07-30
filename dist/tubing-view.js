6d4450cc0dc96aefe524cba32e1983b999505aa1
Matt Insler <matt.insler@gmail.com>
2013-07-29 18:10:29 -0700

    broke up pipelines for http and non-http

--- dist/tubing-view.js
+++ dist/tubing-view.js
@@ -13,50 +13,46 @@
 
   Engines = exports.Engines = require('./engines');
 
-  exports.configure = function(cmd, done) {
-    var content_type, k, _i, _len, _ref, _ref1;
-    _ref = this.config.path;
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      k = _ref[_i];
-      this.config.path[k] = walkabout(this.config.path[k]);
-    }
-    if ((_ref1 = cmd.data) == null) {
-      cmd.data = {};
-    }
-    cmd.parsed = betturl.parse(cmd.req.url);
-    content_type = mime.lookup(cmd.parsed.path);
+  exports.adapt_http_req = function(cmd, done) {
+    var content_type, _base, _ref;
+    cmd.path = cmd.req.url;
+    content_type = mime.lookup(cmd.path);
     if (content_type === 'application/octet-stream') {
       content_type = cmd.req.accepted[0].value;
     }
     cmd.content_type = mime.extension(content_type);
+    if ((_ref = (_base = cmd.req).view) == null) {
+      _base.view = {};
+    }
+    cmd.req.view.pipeline = this.pipeline;
+    cmd.req.view.command = cmd;
     return done();
   };
 
-  exports.resolve_path = function(config_path_name) {
-    return function(cmd, done) {
-      var path,
-        _this = this;
-      path = cmd.parsed.path.replace(new RegExp('\.' + cmd.content_type + '$'), '');
-      return utils.resolve_path_from_root_with_extension(this.config.path[config_path_name], path, cmd.content_type, function(err, content_path) {
-        var engines;
-        if (err != null) {
-          return done(err);
-        }
-        if (content_path == null) {
-          return done();
-        }
-        cmd.resolved = {
-          file: content_path,
-          path: walkabout(content_path.absolute_path.slice(_this.config.path[config_path_name].absolute_path.length))
-        };
-        engines = content_path.absolute_path.slice(content_path.absolute_path.lastIndexOf(path) + path.length);
-        engines = engines.replace(new RegExp('^(/?index)?\.' + cmd.content_type + '\.?'), '').split('.');
-        cmd.engines = engines.filter(function(e) {
-          return (e != null) && e !== '';
-        }).reverse();
+  exports.resolve_path = function(cmd, done) {
+    var path,
+      _this = this;
+    cmd.parsed = betturl.parse(cmd.path);
+    path = cmd.parsed.path.replace(new RegExp('\.' + cmd.content_type + '$'), '');
+    return utils.resolve_path_from_root_with_extension(this.config.path[this.config.resolve_to], path, cmd.content_type, function(err, content_path) {
+      var engines;
+      if (err != null) {
+        return done(err);
+      }
+      if (content_path == null) {
         return done();
-      });
-    };
+      }
+      cmd.resolved = {
+        file: content_path,
+        path: walkabout(content_path.absolute_path.slice(_this.config.path[_this.config.resolve_to].absolute_path.length))
+      };
+      engines = content_path.absolute_path.slice(content_path.absolute_path.lastIndexOf(path) + path.length);
+      engines = engines.replace(new RegExp('^(/?index)?\.' + cmd.content_type + '\.?'), '').split('.');
+      cmd.engines = engines.filter(function(e) {
+        return (e != null) && e !== '';
+      }).reverse();
+      return done();
+    });
   };
 
   exports.fetch_content = function(cmd, done) {
@@ -69,12 +65,28 @@
     });
   };
 
+  exports.create_params = function(cmd, done) {
+    var k, v, _base, _ref, _ref1, _ref2;
+    if ((_ref = cmd.data) == null) {
+      cmd.data = {};
+    }
+    if ((_ref1 = (_base = cmd.data).params) == null) {
+      _base.params = {};
+    }
+    _ref2 = cmd.parsed.query;
+    for (k in _ref2) {
+      v = _ref2[k];
+      cmd.data.params[k] = v;
+    }
+    return done();
+  };
+
   exports.render_engines = function(cmd) {
     var d, step;
     d = this.defer();
     step = function(idx) {
       if (idx === cmd.engines.length) {
-        return d.resolve();
+        return d.resolve(cmd);
       }
       try {
         return Engines.render(cmd.engines[idx], cmd.content, cmd.data, function(err, data) {
@@ -92,6 +104,17 @@
     return d.promise;
   };
 
-  exports.ViewPipeline = tubing.pipeline('View Pipeline').then(exports.configure).then(exports.resolve_path('content')).then(tubing.exit_unless('resolved')).then(exports.fetch_content).then(exports.render_engines);
+  exports.ViewPipeline = tubing.pipeline('View Pipeline').then(exports.resolve_path).then(tubing.exit_unless('resolved')).then(exports.fetch_content).then(exports.create_params).then(exports.render_engines).configure(function(pipeline, config) {
+    var k, _i, _len, _ref, _results;
+    _ref = config.path;
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      k = _ref[_i];
+      _results.push(config.path[k] = walkabout(config.path[k]));
+    }
+    return _results;
+  });
+
+  exports.HttpViewPipeline = tubing.pipeline('Http View Pipeline').then(exports.adapt_http_req).then(exports.ViewPipeline);
 
 }).call(this);
--- lib/tubing-view.coffee
+++ lib/tubing-view.coffee
@@ -6,36 +6,41 @@ walkabout = require 'walkabout'
 utils = exports.utils = require './utils'
 Engines = exports.Engines = require './engines'
 
-exports.configure = (cmd, done) ->
-  @config.path[k] = walkabout(@config.path[k]) for k in @config.path
+exports.adapt_http_req = (cmd, done) ->
+  cmd.path = cmd.req.url
   
-  cmd.data ?= {}
-  
-  cmd.parsed = betturl.parse(cmd.req.url)
-  
-  content_type = mime.lookup(cmd.parsed.path)
+  content_type = mime.lookup(cmd.path)
   content_type = cmd.req.accepted[0].value if content_type is 'application/octet-stream'
   cmd.content_type = mime.extension(content_type)
   
+  cmd.req.view ?= {}
+  cmd.req.view.pipeline = @pipeline
+  cmd.req.view.command = cmd
+  
   done()
 
-exports.resolve_path = (config_path_name) ->
-  (cmd, done) ->
-    path = cmd.parsed.path.replace(new RegExp('\.' + cmd.content_type + '$'), '')
+exports.resolve_path = (cmd, done) ->
+  cmd.parsed = betturl.parse(cmd.path)
   
-    utils.resolve_path_from_root_with_extension @config.path[config_path_name], path, cmd.content_type, (err, content_path) =>
-      return done(err) if err?
-      return done() unless content_path?
+  path = cmd.parsed.path.replace(new RegExp('\.' + cmd.content_type + '$'), '')
+  
+  # console.log 'RESOLVING PATH', @config.resolve_to, @config.path[@config.resolve_to].absolute_path, path, cmd.content_type
+  
+  utils.resolve_path_from_root_with_extension @config.path[@config.resolve_to], path, cmd.content_type, (err, content_path) =>
+    return done(err) if err?
+    return done() unless content_path?
+  
+    cmd.resolved =
+      file: content_path
+      path: walkabout(content_path.absolute_path.slice(@config.path[@config.resolve_to].absolute_path.length))
     
-      cmd.resolved =
-        file: content_path
-        path: walkabout(content_path.absolute_path.slice(@config.path[config_path_name].absolute_path.length))
-      
-      engines = content_path.absolute_path.slice(content_path.absolute_path.lastIndexOf(path) + path.length)
-      engines = engines.replace(new RegExp('^(/?index)?\.' + cmd.content_type + '\.?'), '').split('.')
-      cmd.engines = engines.filter((e) -> e? and e isnt '').reverse()
+    # console.log 'RESOLVED TO', cmd.resolved.path.absolute_path
     
-      done()
+    engines = content_path.absolute_path.slice(content_path.absolute_path.lastIndexOf(path) + path.length)
+    engines = engines.replace(new RegExp('^(/?index)?\.' + cmd.content_type + '\.?'), '').split('.')
+    cmd.engines = engines.filter((e) -> e? and e isnt '').reverse()
+  
+    done()
 
 exports.fetch_content = (cmd, done) ->
   cmd.resolved.file.read_file (err, content) ->
@@ -43,11 +48,18 @@ exports.fetch_content = (cmd, done) ->
     cmd.content = content.toString()
     done()
 
+exports.create_params = (cmd, done) ->
+  cmd.data ?= {}
+  cmd.data.params ?= {}
+  cmd.data.params[k] = v for k, v of cmd.parsed.query
+  
+  done()
+
 exports.render_engines = (cmd) ->
   d = @defer()
   
   step = (idx) ->
-    return d.resolve() if idx is cmd.engines.length
+    return d.resolve(cmd) if idx is cmd.engines.length
     
     try
       Engines.render cmd.engines[idx], cmd.content, cmd.data, (err, data) ->
@@ -62,8 +74,15 @@ exports.render_engines = (cmd) ->
   d.promise
 
 exports.ViewPipeline = tubing.pipeline('View Pipeline')
-  .then(exports.configure)
-  .then(exports.resolve_path('content'))
+  .then(exports.resolve_path)
   .then(tubing.exit_unless('resolved'))
   .then(exports.fetch_content)
+  .then(exports.create_params)
   .then(exports.render_engines)
+  
+  .configure (pipeline, config) ->
+    config.path[k] = walkabout(config.path[k]) for k in config.path
+
+exports.HttpViewPipeline = tubing.pipeline('Http View Pipeline')
+  .then(exports.adapt_http_req)
+  .then(exports.ViewPipeline)
